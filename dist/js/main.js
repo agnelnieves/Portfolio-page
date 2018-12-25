86a8d03b9697b7f7057cbb5924718bd9d8c7c44f
Agnel Nieves <agnelnieves@gmail.com>
2018-12-24 23:54:41 -0500

    feat: Menu button animation
    - Add menu partial styles
    - Add menu button animation
    - Add JavaScript for interaction

---
+++ dist/js/main.js
@@ -0,0 +1,37 @@
+// Select DOM Items (HTML tags, attributes, etc)
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+// Selecting all menu items
+const navItems = document.querySelectorAll('.nav-item');
+
+// Set Initial State of Menu
+let showMenu = false;
+
+// Detect mouse click
+menuBtn.addEventListener('click', toggleMenu);
+
+// menu opener/hider function
+function toggleMenu() {
+    if(!showMenu){
+        // adding classes
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+
+        // Set menu state
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        // Set menu state
+        showMenu = false;
+    }
+}
